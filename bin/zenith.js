c4d9f800bbb1872c65c7e75fc2e0d478d3975f3e
Judah Sullivan <judahsullivan.dev@gmail.com>
2026-02-04 11:52:48 -0600

    fix(cli): convert to ESM and update tools to v1.3.2

--- bin/zenith.js
+++ bin/zenith.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { createZenithBundler } = require('@zenithbuild/bundler');
+import { createZenithBundler } from '@zenithbuild/bundler';
 
 const args = process.argv.slice(2);
 const command = args[0];
