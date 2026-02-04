d4c2b192bae727fa0fd93d1ccc7027a573b0a3fa
Judah Sullivan <judahsullivan.dev@gmail.com>
2026-02-04 02:02:34 -0600

    chore: migrate to unified core architecture v1.3.1

---
+++ bin/zenith.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const { createZenithBundler } = require('@zenithbuild/bundler');
+
+const args = process.argv.slice(2);
+const command = args[0];
+const root = process.cwd();
+
+(async () => {
+    const bundler = createZenithBundler();
+
+    try {
+        if (command === 'dev') {
+            await bundler.dev({ root });
+        } else if (command === 'build') {
+            await bundler.build({ root });
+        } else {
+            console.log("Usage: zenith [dev|build]");
+            process.exit(1);
+        }
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+})();
